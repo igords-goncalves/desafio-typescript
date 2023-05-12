6d68a7a3bc312dbe5568e9ed00875f770548788f
Igor Gonçalves <igordossantos38@gmail.com>
2023-05-11 22:47:29 -0300

    simple commit

--- desafio.js
+++ desafio.js
@@ -1,23 +1,39 @@
 "use strict";
-//%* Exercício 1 - Classe
+// Exercício 1 - Classe
+// Atributos: nome, preco e desconto
+// Criar contructor
+// Obs 1.: Desconto é opcional (valor padrão = 0)
+// Obs 2.: Criar produtos: passando dois e três params
+class Product {
+    constructor(nome, preco, desconto = 0) {
+        this.nome = nome;
+        this.preco = preco;
+        this.desconto = desconto;
+        this.precoComDesconto = (desconto, preco) => {
+            return desconto * 100 - preco;
+        };
+    }
+}
+const produto1 = new Product("Camiseta", 28.99, 0.5);
+console.log(`R$${produto1.precoComDesconto(produto1.desconto, produto1.preco)}`);
 class Moto {
     constructor(nome, velocidade = 0) {
         this.nome = nome;
         this.velocidade = velocidade;
         this.buzinar = () => {
-            console.log('Toooooooooot!');
+            console.log("Toooooooooot!");
         };
         this.acelerar = (delta) => {
-            return this.velocidade = this.velocidade + delta;
+            return (this.velocidade = this.velocidade + delta);
         };
     }
 }
-const moto = new Moto('Ducati');
+const moto = new Moto("Ducati");
 moto.buzinar();
 console.log(moto.velocidade);
 moto.acelerar(30);
 console.log(moto.velocidade);
-//%* Exercício 2 - Herança
+// Exercício 2 - Herança
 class Objeto2D {
     constructor(base = 0, altura = 0) {
         this.base = base;
@@ -32,12 +48,12 @@ class Retangulo extends Objeto2D {
         };
     }
 }
-const r1 = new Retangulo;
+const r1 = new Retangulo();
 console.log(`Area = ${r1.area()}`);
-//%* Exercício 3 - Getters & Setters
+// Exercício 3 - Getters & Setters
 class Estagiario {
     constructor() {
-        this._primeiroNome = '';
+        this._primeiroNome = "";
     }
     get primeiroNome() {
         return this._primeiroNome;
@@ -47,16 +63,16 @@ class Estagiario {
             this._primeiroNome = v;
         }
         else {
-            this._primeiroNome = '';
+            this._primeiroNome = "";
         }
     }
 }
-const est1 = new Estagiario;
+const est1 = new Estagiario();
 console.log(est1.primeiroNome);
-est1.primeiroNome = 'Le';
+est1.primeiroNome = "Le";
 console.log(est1.primeiroNome);
-est1.primeiroNome = 'Leoni';
+est1.primeiroNome = "Leoni";
 console.log(est1.primeiroNome);
-est1.primeiroNome = 'Leonardo';
+est1.primeiroNome = "Leonardo";
 console.log(est1.primeiroNome);
 //# sourceMappingURL=desafio.js.map
--- desafio.ts
+++ desafio.ts
@@ -1,70 +1,85 @@
-//%* Exercício 1 - Classe
+// Exercício 1 - Classe
 
-class Moto {
+// Atributos: nome, preco e desconto
+// Criar contructor
+// Obs 1.: Desconto é opcional (valor padrão = 0)
+// Obs 2.: Criar produtos: passando dois e três params
+
+class Product {
     constructor(
         public nome: string,
-        public velocidade: number = 0) {
-    }
-    public buzinar = (): void => {
-        console.log('Toooooooooot!')
-    }
-    public acelerar = (delta: number): number => {
-        return this.velocidade = this.velocidade + delta
-    }
+        public preco: number,
+        public desconto: number = 0
+    ) {}
+    public precoComDesconto = (desconto: number, preco: number): number => {
+        return desconto * 100 - preco;
+    };
 }
- 
-const moto: Moto = new Moto('Ducati')
-moto.buzinar()
-console.log(moto.velocidade)
-moto.acelerar(30)
-console.log(moto.velocidade)
- 
-//%* Exercício 2 - Herança
+
+const produto1 = new Product("Camiseta", 28.99, 0.5);
+console.log(
+    `R$${produto1.precoComDesconto(produto1.desconto, produto1.preco)}`
+);
+
+class Moto {
+    constructor(public nome: string, public velocidade: number = 0) {}
+    public buzinar = (): void => {
+        console.log("Toooooooooot!");
+    };
+    public acelerar = (delta: number): number => {
+        return (this.velocidade = this.velocidade + delta);
+    };
+}
+
+const moto: Moto = new Moto("Ducati");
+moto.buzinar();
+console.log(moto.velocidade);
+moto.acelerar(30);
+console.log(moto.velocidade);
+
+// Exercício 2 - Herança
 
 class Objeto2D {
-    constructor (
-        public base: number =  0,
-        public altura: number =  0) {
-    }
+    constructor(public base: number = 0, public altura: number = 0) {}
 }
- 
+
 class Retangulo extends Objeto2D {
     constructor() {
-        super(5, 7)
+        super(5, 7);
     }
     public area = (): number => {
-        return this.base * this.altura
-    }
+        return this.base * this.altura;
+    };
 }
-const r1: Retangulo = new Retangulo
-console.log(`Area = ${r1.area()}`)
- 
-//%* Exercício 3 - Getters & Setters
+const r1: Retangulo = new Retangulo();
+console.log(`Area = ${r1.area()}`);
+
+// Exercício 3 - Getters & Setters
 
 class Estagiario {
-    private _primeiroNome: string =  ''
+    private _primeiroNome: string = "";
 
-    public get primeiroNome() : string {
-        return this._primeiroNome
+    public get primeiroNome(): string {
+        return this._primeiroNome;
     }
 
-    public set primeiroNome(v : string) {
-        if(v.length >= 3) {
-            this._primeiroNome = v
+    public set primeiroNome(v: string) {
+        if (v.length >= 3) {
+            this._primeiroNome = v;
         } else {
-            this._primeiroNome = ''
+            this._primeiroNome = "";
         }
     }
 }
 
-const est1: Estagiario = new Estagiario
+const est1: Estagiario = new Estagiario();
 
-console.log(est1.primeiroNome)
-est1.primeiroNome = 'Le'
-console.log(est1.primeiroNome)
+console.log(est1.primeiroNome);
+est1.primeiroNome = "Le";
+console.log(est1.primeiroNome);
 
-est1.primeiroNome = 'Leoni'
-console.log(est1.primeiroNome)
+est1.primeiroNome = "Leoni";
+console.log(est1.primeiroNome);
 
-est1.primeiroNome = 'Leonardo'
-console.log(est1.primeiroNome)
+est1.primeiroNome = "Leonardo";
+console.log(est1.primeiroNome);
